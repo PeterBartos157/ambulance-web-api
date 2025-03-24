d2ffad3801f7c152237ab4db679a67cf7840adaa
Peter Bartoš <xbartosp2@stuba.sk>
2025-03-24 12:12:10 +0100

    ambulance waiting list

--- internal/ambulance_wl/README.md
+++ internal/ambulance_wl/README.md
@@ -13,7 +13,7 @@ To see how to make this your own, look here:
 [README](https://openapi-generator.tech)
 
 - API version: 1.0.0
-- Build date: 2025-03-24T10:14:47.437873225Z[Etc/UTC]
+- Build date: 2025-03-24T10:42:56.542252798Z[Etc/UTC]
 - Generator version: 7.13.0-SNAPSHOT
 
 ### Running the server
--- internal/ambulance_wl/model_condition.go
+++ internal/ambulance_wl/model_condition.go
@@ -12,13 +12,12 @@ package ambulance_wl
 
 // Condition - Describes disease, symptoms, or other reasons of patient   visit
 type Condition struct {
-
 	Value string `json:"value"`
 
 	Code string `json:"code,omitempty"`
 
 	// Link to encyclopedical explanation of the patient's condition
-	Reference string `json:"reference,omitempty"`
+	string `json:"reference,omitempty"`
 
 	TypicalDurationMinutes int32 `json:"typicalDurationMinutes,omitempty"`
 }
